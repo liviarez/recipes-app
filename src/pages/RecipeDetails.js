6cde764b3161f523367b8e3ac6cca60eabc942e6
Gabriel do Carmo Assis <gabrieleboltffggff@gmail.com>
2023-07-10 23:40:00 -0300

    arrumar o video do youtube e fazer um array com os ingredientes

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useRouteMatch } from 'react-router-dom';
+import Fetch from '../functions/Fetch';
 
 function RecipeDetails() {
   const { idReceita } = useParams();
@@ -11,28 +12,67 @@ function RecipeDetails() {
   useEffect(() => {
     const fetchRecipe = async () => {
       if (type === 'meals') {
-        const data = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idReceita}`)
-          .then((response) => response.json());
-        setRecipe(data);
-        console.log(type);
+        const data = await Fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idReceita}`);
+        console.log(data);
+        setRecipe(data.meals);
       }
       if (type === 'drinks') {
-        const data = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idReceita}`)
-          .then((response) => response.json());
-        setRecipe(data);
-        console.log(type);
+        const data = await Fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idReceita}`);
+        console.log(data);
+        setRecipe(data.drinks);
       }
     };
 
     fetchRecipe();
   }, []);
 
-  console.log(recipe);
+  // console.log(recipe);
 
   return (
     <div>
-      { idReceita }
+      {recipe.length === 0 && <h1>Carregando...</h1>}
+      {recipe.length > 0 && type === 'meals'
+        && (
+          <div>
+            <p data-testid="recipe-title">{ recipe[0].strMeal }</p>
+            <p data-testid="recipe-category">{ recipe[0].strCategory }</p>
+            {/* <p
+              data-testid={ `${index}-ingredient-name-and-measure` }
+            >
+              { recipe[0].strMeal }
+
+            </p> */}
+            <p data-testid="instructions">{ recipe[0].strInstructions }</p>
+            <img
+              alt="foto drink"
+              src={ recipe[0].strMealThumb }
+              data-testid="recipe-photo"
+            />
+            <iframe
+              width="560"
+              height="315"
+              src={ recipe[0].strYoutube }
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer;
+                autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+              data-testid="video"
+            />
+          </div>
+        )}
+      {recipe.length > 0 && type === 'drinks'
+        && (
+          <div>
+            <img
+              alt="foto drink"
+              src={ recipe[0].strDrinkThumb }
+              data-testid="recipe-photo"
+            />
+          </div>
+        )}
     </div>
+
   );
 }
 
