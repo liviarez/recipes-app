4a91ee693836aab891d16aacced08caccc6533e7
Seu nome <yago_bohmcruz@hotmail.com>
2023-07-11 08:07:34 -0300

    Req 37 - :wrench:

--- src/components/RecipeInProgress.js
+++ src/components/RecipeInProgress.js
@@ -1,17 +1,5 @@
 import React from 'react';
 
-// 37 - Desenvolva a tela de modo que contenha uma imagem da receita, o título, a categoria em caso de comidas e se é ou não alcoólico em caso de bebidas, uma lista de ingredientes com suas respectivas quantidades e instruções
-// Observações técnicas
-// Verifica se os atributos data-testid estão presentes na tela:
-
-// A foto deve possuir o atributo data-testid="recipe-photo"; ✅
-// O título deve possuir o atributo data-testid="recipe-title"; ✅
-// O botão de compartilhar deve possuir o atributo data-testid="share-btn"; ✅
-// O botão de favoritar deve possuir o atributo data-testid="favorite-btn"; ✅
-// O texto da categoria deve possuir o atributo data-testid="recipe-category"; ✅
-// O elemento de instruções deve possuir o atributo data-testid="instructions"; ✅
-// O botão para finalizar a receita deve possuir o atributo data-testid="finish-recipe-btn".✅
-
 export default function RecipeInProgress() {
   return (
     <div>
