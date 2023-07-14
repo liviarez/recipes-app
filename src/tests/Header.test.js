4afbd42cd624f61563b70743131d3718d8725a28
Seu nome <yago_bohmcruz@hotmail.com>
2023-07-14 00:51:53 -0300

    Testes: Implementando novos testes :bug: :test_tube:

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,139 +1,39 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import { BrowserRouter as Router, useHistory } from 'react-router-dom';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import { renderWithRouter } from '../helpers/renderWithRouter';
 import Header from '../components/Header';
 
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useHistory: jest.fn(),
-}));
+describe('Verifica as funcionalidades do componente Header', () => {
+  it('Verifica se o componente é renderizado na tela', () => {
+    renderWithRouter(<Header />);
+    const header = screen.getByTestId('profile-top-btn');
+    expect(header).toBeInTheDocument();
+  });
 
-describe('Header', () => {
-  const prflTopBtn = 'profile-top-btn';
-  const srchTopBtn = 'search-top-btn';
-
-  test('renders profile and search icons', () => {
-    const { getByTestId } = render(
-      <Router>
-        <Header />
-      </Router>,
-    );
-
-    const profileIcon = getByTestId(prflTopBtn);
-    const searchIcon = getByTestId(srchTopBtn);
-
+  it('Verifica se o componente renderiza o icone de perfil e o titulo', () => {
+    renderWithRouter(<Header />);
+    const profileIcon = screen.getByTestId('profile-top-btn');
+    const pageTitle = screen.getByTestId('page-title');
     expect(profileIcon).toBeInTheDocument();
-    expect(searchIcon).toBeInTheDocument();
+    expect(pageTitle).toBeInTheDocument();
   });
 
-  test('renders page title based on current route', () => {
-    const routes = [
-      { path: '/meals', title: 'Meals' },
-      { path: '/drinks', title: 'Drinks' },
-      { path: '/profile', title: 'Profile' },
-      { path: '/done-recipes', title: 'Done Recipes' },
-      { path: '/favorite-recipes', title: 'Favorite Recipes' },
-      { path: '/', title: '' },
-    ];
+  // it('Verifica se a barra de pesquisa aparece na tela quando clicado no botão', () => {
+  //   renderWithRouter(<Header />);
+  //   const searchButton = screen.getByRole('img', { name: /search/i });
+  //   userEvent.click(searchButton);
 
-    routes.forEach(({ path, title }) => {
-      const { getByTestId } = render(
-        <Router initialEntries={ [path] }>
-          <Header />
-        </Router>,
-      );
+  //   const searchBar = screen.getByTestId('search-input');
+  //   expect(searchBar).toBeInTheDocument();
+  // });
+  // it('Verifica se o texto do título é renderizado corretamente com base na rota atual', () => {
+  //   renderWithRouter(<Header />, { initialEntries: ['/meals'] });
+  //   const pageTitleMeals = screen.getByTestId('page-title');
+  //   expect(pageTitleMeals.textContent).toBe('Meals');
 
-      const pageTitle = getByTestId('page-title');
-      expect(pageTitle.textContent).toBe(title);
-    });
-  });
-
-  test('displays search input when search button is clicked', () => {
-    const { getByTestId } = render(
-      <Router>
-        <Header />
-      </Router>,
-    );
-
-    const searchButton = getByTestId(srchTopBtn);
-    fireEvent.click(searchButton);
-
-    const searchInput = getByTestId('search-input');
-    expect(searchInput).toBeInTheDocument();
-  });
-
-  test('hides search input when search button is clicked again', () => {
-    const { getByTestId, queryByTestId } = render(
-      <Router>
-        <Header />
-      </Router>,
-    );
-
-    const searchButton = getByTestId(srchTopBtn);
-    fireEvent.click(searchButton);
-
-    fireEvent.click(searchButton);
-
-    const searchInput = queryByTestId('search-input');
-    expect(searchInput).not.toBeInTheDocument();
-  });
-
-  test('redirects to profile screen and changes page title when profile button is clicked', () => {
-    const mockHistoryPush = jest.fn();
-    useHistory.mockReturnValueOnce({
-      push: mockHistoryPush,
-    });
-
-    const { getByTestId } = render(
-      <Router>
-        <Header />
-      </Router>,
-    );
-
-    const profileButton = getByTestId(prflTopBtn);
-    fireEvent.click(profileButton);
-
-    expect(mockHistoryPush).toHaveBeenCalledWith('/profile');
-
-    const pageTitle = getByTestId('page-title');
-    expect(pageTitle.textContent).toBe('Profile');
-  });
-
-  test('redirects to profile screen when profile button is clicked', () => {
-    const mockHistoryPush = jest.fn();
-    useHistory.mockReturnValueOnce({
-      push: mockHistoryPush,
-    });
-
-    const { getByTestId } = render(
-      <Router>
-        <Header />
-      </Router>,
-    );
-
-    const profileButton = getByTestId(prflTopBtn);
-    fireEvent.click(profileButton);
-
-    expect(mockHistoryPush).toHaveBeenCalledWith('/profile');
-  });
-
-  test('does not display search icon on non-meals/drinks routes', () => {
-    const routes = [
-      '/profile',
-      '/done-recipes',
-      '/favorite-recipes',
-      '/search',
-    ];
-
-    routes.forEach((route) => {
-      const { queryByTestId } = render(
-        <Router initialEntries={ [route] }>
-          <Header />
-        </Router>,
-      );
-
-      const searchIcon = queryByTestId('search-top-btn');
-      expect(searchIcon).not.toBeInTheDocument();
-    });
-  });
+  //   renderWithRouter(<Header />, { initialEntries: ['/drinks'] });
+  //   const pageTitleDrinks = screen.getByTestId('page-title');
+  //   expect(pageTitleDrinks.textContent).toBe('Drinks');
+  // });
 });
---
+++ src/tests/SearchBar.test.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { renderWithRouter } from '../helpers/renderWithRouter';
+import SearchBar from '../components/SearchBar';
+
+const testIdSearchInput = 'search-input';
+const searchBtn = 'exec-search-btn';
+const mockCardName = '0-card-name';
+
+describe('Verifica as funcionalidades do componente SearchBar', () => {
+  it('Verifica se o campo de pesquisa é renderizado na tela', () => {
+    renderWithRouter(<SearchBar />);
+    const searchInput = screen.getByTestId(testIdSearchInput);
+    expect(searchInput).toBeInTheDocument();
+  });
+
+  it('Verifica se os tipos de busca são renderizados corretamente', () => {
+    renderWithRouter(<SearchBar />);
+    const ingredientSearch = screen.getByTestId('ingredient-search-radio');
+    const nameSearch = screen.getByTestId('name-search-radio');
+    const firstLetterSearch = screen.getByTestId('first-letter-search-radio');
+    expect(ingredientSearch).toBeInTheDocument();
+    expect(nameSearch).toBeInTheDocument();
+    expect(firstLetterSearch).toBeInTheDocument();
+  });
+
+  it('Verifica se a função de busca é chamada corretamente', () => {
+    renderWithRouter(<SearchBar />);
+    const searchButton = screen.getByTestId(searchBtn);
+    const searchInput = screen.getByTestId(testIdSearchInput);
+
+    userEvent.type(searchInput, 'banana');
+    userEvent.click(searchButton);
+  });
+
+  it('Verifica se a função de busca por ingrediente é chamada corretamente', async () => {
+    renderWithRouter(<SearchBar />);
+    const ingredientSearch = screen.getByTestId('ingredient-search-radio');
+    const searchButton = screen.getByTestId(searchBtn);
+    const searchInput = screen.getByTestId(testIdSearchInput);
+
+    userEvent.click(ingredientSearch);
+    userEvent.type(searchInput, 'banana');
+    userEvent.click(searchButton);
+
+    const cardName = await screen.findByTestId(mockCardName);
+
+    expect(cardName.textContent).toBe('Banana Pancakes');
+  });
+
+  it('Verifica se a função de busca por nome é chamada corretamente', async () => {
+    renderWithRouter(<SearchBar />);
+    const nameSearch = screen.getByTestId('name-search-radio');
+    const searchButton = screen.getByTestId(searchBtn);
+    const searchInput = screen.getByTestId(testIdSearchInput);
+
+    userEvent.click(nameSearch);
+    userEvent.type(searchInput, 'chocolate');
+    userEvent.click(searchButton);
+
+    const cardName = await screen.findByTestId(mockCardName);
+
+    expect(cardName.textContent).toBe('Chocolate Gateau');
+  });
+
+  it('Verifica se a função de busca por primeira letra é chamada corretamente', async () => {
+    renderWithRouter(<SearchBar />);
+    const firstLetterSearch = screen.getByTestId('first-letter-search-radio');
+    const searchButton = screen.getByTestId(searchBtn);
+    const searchInput = screen.getByTestId(testIdSearchInput);
+
+    userEvent.click(firstLetterSearch);
+    userEvent.type(searchInput, 'a');
+    userEvent.click(searchButton);
+
+    const cardName = await screen.findByTestId(mockCardName);
+
+    expect(cardName.textContent).toBe('Apple Frangipan Tart');
+  });
+
+  it('Verifica se a barra de pesquisa aparece após clicar no botão de busca', () => {
+    renderWithRouter(<SearchBar />);
+    const searchButton = screen.getByTestId(searchBtn);
+
+    userEvent.click(searchButton);
+
+    const searchInput = screen.getByTestId(testIdSearchInput);
+    expect(searchInput).toBeVisible();
+  });
+  // it('Verifica se o alerta é exibido corretamente para pesquisa com apenas um caractere', () => {
+  //   const alertMock = jest.spyOn(global, 'alert');
+  //   renderWithRouter(<SearchBar />);
+  //   const searchButton = screen.getByTestId('exec-search-btn');
+
+  //   userEvent.click(searchButton);
+
+  //   expect(alertMock).toHaveBeenCalledWith('Your search must have only 1 (one) character');
+
+  //   alertMock.mockRestore();
+  // });
+});
