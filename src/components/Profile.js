f7cce239206cde4cebcf02791566a23425227bcf
Nina Palma <ninapalmadev@gmail.com>
2023-07-17 17:47:34 -0300

    requisito 57

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,7 +6,25 @@ function Profile() {
   return (
     <div>
       <Header />
-      <div>Profile</div>
+      <h3 data-testid="profile-email">email</h3>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+      >
+        Favorite Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+      >
+        Logout
+      </button>
       <Footer />
     </div>
 
